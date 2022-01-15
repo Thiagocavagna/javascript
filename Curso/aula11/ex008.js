c72855caef872bf417f2f622037cb7e308abfe33
Thiago Cavagna <97311759+Thiagocavagna@users.noreply.github.com>
2022-01-15 18:55:06 -0300

    ex11 add

---
+++ Curso/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu caro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log('Diriga sempre usando cinto de segurança')
