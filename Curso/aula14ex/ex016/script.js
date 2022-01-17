9cb9760d322a8ca29a65da2e8a54a4daa4dc4cb8
Thiago Cavagna <97311759+Thiagocavagna@users.noreply.github.com>
2022-01-17 16:47:21 -0300

    ex 16 atualizado

---
+++ Curso/aula14ex/ex016/script.js
@@ -0,0 +1,6 @@
+function contar() {
+    var inicio = document.getElementById('inicio').value
+    var fim = document.getElementById('fim').value
+    var passo = document.getElementById('passo').value
+    var res = document.getElementById('res')
+
