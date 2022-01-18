8d2364a1af14730e0d32b1e5a5485eade9aa73be
Thiago Cavagna <97311759+Thiagocavagna@users.noreply.github.com>
2022-01-18 17:02:29 -0300

    aula 15 add

---
+++ Curso/aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [2, 7, 3, 4, 1]
+
+num.push(7)
+
+num.sort()
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
