fb79d5a4d55538e7a0dcc7e834eb609841316cb9
Thiago Cavagna <97311759+Thiagocavagna@users.noreply.github.com>
2022-01-20 10:06:41 -0300

    atualizado

--- Curso/aula16ex/exfinal/script.js
+++ Curso/aula16ex/exfinal/script.js
@@ -3,33 +3,73 @@ var sel = document.getElementById('sel')
 //var n = Number(txtn.value)
 var numero = []
 
+//Function com parametros
+function soma(n){
+    let soma = 0
+
+    for(c = 0; c < n.length; c++) {
+        soma = soma + n[c] 
+    }
+    return soma
+}
+
+function media(n){
+    let media = 0
+    for(c = 0; c <n.length; c++) {
+        media = soma(numero) / numero.length
+    }
+    return media
+}
+
+//Function sem parametros
+
 function adicionar(){
-    var n = Number(txtn.value)
+    let n = Number(txtn.value)
+
 
     if (n < 1 || n > 100) {
         alert('Preencha um número válido!')
     } else {
-        var item = document.createElement('option')
+        let item = document.createElement('option')
         item.text = `Valor ${n} adicionado.`
         sel.appendChild(item)
         numero.push(n)
         numero.sort()
-        var item2 = document.createElement('option')
-        item2.text = `${numero}`
-        sel.appendChild(item2)
-       
+ 
     }
     
 }
 
-function finalizar(){
 
+function finalizar(){
+    let res = [document.getElementById('res1'), document.getElementById('res2'), document.getElementById('res3'), document.getElementById('res4'), document.getElementById('res5')]
+    let min = Math.min(...numero)
+    let max = Math.max(...numero)
+   
+
+    if (sel.length == 0) {
+        alert('Adicione um número por favor.')
+    } else {
+        res[0].innerHTML = `Ao todo temos ${numero.length} números cadastrados.`
+        res[1].innerHTML = `O maior valor informado foi ${max}.`
+        res[2].innerHTML = `O menor valor informado foi ${min}.`
+        res[3].innerHTML = `Somando todos os valores, temos ${soma(numero)}.`
+        res[4].innerHTML = `A média dos valores digitados é ${media(numero)}.`
+}
+    
+    
 }
 
 function limpar (){
     sel.innerHTML = ''
+    res.innerHTML = ''
 }
 
-function testar(){
-    alert(`Teste`)
-}
+
+
+/*let soma = 0
+
+    for(c = 0; c < numero.length; c++) {
+        soma = soma + numero[c] 
+    }
+    */
