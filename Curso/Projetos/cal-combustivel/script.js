2597e2093bffe5220d1a6342ea49ac52ac5e5f1b
Thiagocavagna <97311759+Thiagocavagna@users.noreply.github.com>
2022-04-04 15:51:46 -0300

    calc combustivel

--- Curso/Projetos/cal-combustivel/script.js
+++ Curso/Projetos/cal-combustivel/script.js
@@ -8,10 +8,10 @@ function calcular() {
     if (c.a.value == 0 || c.g.value == 0) {
         alert('Por favor, preencha todos os campos corretamente')
     } else if (valor < 0.7) {
-        res.innerHTML = `<p>O resultado da divisão foi ${(valor*100).toFixed(2)}%. Hoje compensa abastecer com <strong>álcool</strong>.</p>`
+        res.innerHTML = `<p>O resultado foi de ${(valor*100).toFixed(2)}%. Hoje compensa abastecer com <strong>álcool</strong>.</p>`
     } else if (valor.toFixed(2) > 0.7) {
-        res.innerHTML = `<p>O resultado da divisão foi ${(valor*100).toFixed(2)}%. Hoje compensa abaster com <strong>Gasolina</strong>.</p>`
+        res.innerHTML = `<p>O resultado foi de ${(valor*100).toFixed(2)}%. Hoje compensa abaster com <strong>Gasolina</strong>.</p>`
     } else {
-        res.innerHTML = `<p>O resultado da divisão foi ${(valor*100).toFixed(2)}%. Hoje os combustíveis estão com o mesmo custo benefício.</p>`
+        res.innerHTML = `<p>O resultado foi de ${(valor*100).toFixed(2)}%. Hoje os combustíveis estão com o mesmo custo benefício.</p>`
     }
 }
